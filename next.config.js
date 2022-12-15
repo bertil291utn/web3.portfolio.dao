1c16e1f129924706a05508e887cba135fdd98430
Bertil Tandayamo <bertilt4@gmail.com>
2022-12-15 18:49:28 -0500

    - add sass - move to src folder

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack(config) {
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
