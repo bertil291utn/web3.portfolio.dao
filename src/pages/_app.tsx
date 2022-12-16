237d9ebdd888cd2dff5917c1e3fa49f7bf70ce4e
Bertil Tandayamo <bertilt4@gmail.com>
2022-12-16 18:51:41 -0500

    - add favicon and colors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,21 +10,27 @@ import { chainProv, client } from '@utils/web3';
 import WalletProvider from '@context/WalletProvider';
 import type { AppProps } from 'next/app'
 import '../css/global.scss';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <WalletProvider>
-      <WagmiConfig client={client}>
-        <RainbowKitProvider chains={chainProv}>
-          <ThemeProvider>
-            <NavbarComponent navbarElements={navbarElements} />
-            <div className='content'>
-              <Component {...pageProps} />
-            </div>
-            <FooterComponent />
-          </ThemeProvider>
-        </RainbowKitProvider>
-      </WagmiConfig>
-    </WalletProvider>
+    <>
+      <Head>
+        <title>Web3 DAO</title>
+      </Head>
+      <WalletProvider>
+        <WagmiConfig client={client}>
+          <RainbowKitProvider chains={chainProv}>
+            <ThemeProvider>
+              <NavbarComponent navbarElements={navbarElements} />
+              <div className='content'>
+                <Component {...pageProps} />
+              </div>
+              <FooterComponent />
+            </ThemeProvider>
+          </RainbowKitProvider>
+        </WagmiConfig>
+      </WalletProvider>
+    </>
   )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-//TODO-WIP: add fav icon and title
 export default function Document() {
   return (
     <Html lang="en">
@@ -8,6 +7,9 @@ export default function Document() {
           href='https://fonts.googleapis.com/css2?family=Mulish:wght@200;300;400;500;600;700;800;900;1000&display=optional'
           rel='stylesheet'
         />
+        <meta name='theme-color' content={'#fcded1'} />
+        <link rel='icon' type='image/svg+xml' href={'/favicon.svg'} />
+        
       </Head>
       <body>
         <Main />
