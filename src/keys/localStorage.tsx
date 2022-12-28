d2945d6fb9db778b019cff9187ffb0f033ee00c5
Bertil Tandayamo <bertilt4@gmail.com>
2022-12-28 18:47:41 -0500

    - remove unused functions and imports

--- src/keys/localStorage.tsx
+++ src/keys/localStorage.tsx
@@ -1,5 +1,4 @@
 export const localStorageKeys = {
-  isWeb3User: 'isWeb3User',
   isFirstTime: 'isFirstTime',
   walletConnected: 'wagmi.connected',
   claimingTxHash: 'claimingTxHash',
--- src/utils/firebaseFunctions.tsx
+++ src/utils/firebaseFunctions.tsx
@@ -4,11 +4,6 @@ import { Firebase } from '@utils/firebase'
 const firebase = new Firebase();
 const db = firebase.firestore();
 
-export const getIPTable = (walletAddress: string) => {
-  if (!walletAddress) return;
-  return db.collection(collections.wallets).doc(walletAddress).get();
-}
-
 export const addNewDevice = (walletAddresID: string, body: IPs) => {
   if (!walletAddresID) {
     throw new Error("Can't be empty wallet address")
