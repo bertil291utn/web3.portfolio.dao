7ac8ac356b14a7f5a6ad2ba1ba58377fd6be4f14
Bertil Tandayamo <bertilt4@gmail.com>
2023-02-08 23:01:49 -0500

    - add missing props text area - fix small theme conversion - style text area comp

--- src/components/common/TextArea.component.tsx
+++ src/components/common/TextArea.component.tsx
@@ -1,38 +1,58 @@
+import { useTheme } from 'next-themes';
 import { useEffect, useRef, useState } from 'react';
+import { FiSend } from 'react-icons/fi';
 import styles from './TextArea.module.scss';
 
 interface Props {
   name: string
   value: string
   onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void
+  onSubmit?: () => void
   id?: string
   className?: string
   placeholder?: string
+  icon?: boolean
 }
 
-const TextArea = ({ className, name, id, placeholder, value, onChange }: Props) => {
+const TextArea = ({ className, name, id, placeholder, value, onChange, icon, onSubmit }: Props) => {
 
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  const [themeClass, setThemeClass] = useState('');
 
   const resizeTextArea = () => {
     if (!textAreaRef.current) return;
     textAreaRef.current.style.height = "auto";
     textAreaRef.current.style.height = textAreaRef.current.scrollHeight + "px";
+
   };
 
-  useEffect(() => { value && resizeTextArea() }, [value]);
+  useEffect(() => {
+    resizeTextArea();
+  }, [value]);
 
+  const { resolvedTheme } = useTheme();
+
+  useEffect(() => {
+    setThemeClass(resolvedTheme === 'dark' ? styles['icon-container__dark'] : styles['icon-container__light']);
+  }, [resolvedTheme])
 
   return (
-    <textarea
-      ref={textAreaRef}
-      value={value}
-      onChange={onChange}
-      placeholder={placeholder || 'Input textarea'}
-      className={`${className || ''} ${styles['text-area']}`}
-      name={name}
-      id={id}
-    ></textarea>
+    <div className={styles['txa-content']}>
+      <textarea
+        ref={textAreaRef}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder || 'Input textarea'}
+        className={`${className || ''} ${styles['text-area']}`}
+        name={name}
+        id={id}
+      >
+      </textarea>
+
+      {icon && <button className={`${styles['icon-container']} ${themeClass}`} onClick={onSubmit}>
+        <FiSend className={styles['icon']} />
+      </button>}
+    </div>
   );
 }
 
--- src/layouts/MintUserNFT.component.tsx
+++ src/layouts/MintUserNFT.component.tsx
@@ -6,22 +6,29 @@ import styles from './MintUserNFT.module.scss'
 const MintUserNFT = () => {
   const [NFTName, setNFTName] = useState<string>();
   const [NFTDescription, setNFTDescription] = useState<string>();
+
+  const sendPrompt = () => {
+    if (NFTDescription && NFTDescription.length < 3) return;
+    console.log('send prompt', NFTDescription)
+  }
   return (
     <div className={styles['container']}>
       <div className={styles["nft-content"]}>
-        <InputComponent
+        {/* <InputComponent
           className={styles['input-nft-name']}
           name='NFTName'
           placeholder='Sunflowers painting'
           value={NFTName || ''}
           onChange={(e) => setNFTName(e.target.value)}
-        />
+        /> */}
 
         <TextArea
           name='NFTDescription'
           placeholder='An Impressionist oil painting of sunflowers in a purple vase...'
           value={NFTDescription || ''}
           onChange={(e) => setNFTDescription(e.target.value)}
+          onSubmit={sendPrompt}
+          icon
         />
       </div>
     </div>);
