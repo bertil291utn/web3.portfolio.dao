ca05d46f97bf376fdcd2b632d4fe02ea3a5fcbcc
Bertil Tandayamo <bertilt4@gmail.com>
2023-02-10 09:26:50 -0500

    - add placeholders for generate  image section - add generate image section

--- src/layouts/MintUserNFT.component.tsx
+++ src/layouts/MintUserNFT.component.tsx
@@ -1,7 +1,9 @@
 import Chip from '@components/common/Chip.component';
 import InputComponent from '@components/common/Input.component';
+import SectionPanel from '@components/common/SectionPanel.component';
 import Subtitle from '@components/common/Subtitle.component';
 import TextArea from '@components/common/TextArea.component';
+import { generateLabels } from '@placeholders/home-mint.placeholders';
 import { mintPrompts } from '@placeholders/mint-prompts-examples.placeholders';
 import { countNumberLetters } from '@utils/common';
 import Image from 'next/image';
@@ -21,51 +23,59 @@ const MintUserNFT = () => {
   return (
     <div className={styles['container']}>
       <div className={styles["nft-content"]}>
-        {/* <InputComponent
+
+        <SectionPanel
+          id={generateLabels.id}
+          title={generateLabels.title}
+          subtitle={generateLabels.subtitle}
+        >
+
+          <div className={styles["image-content"]}>
+            <Image
+              src={"https://preview.redd.it/an-avocado-armchair-v0-uooc9auz6qs81.png?auto=webp&s=bf5227eca8895def1f055e6407a6f5da5dbee3a7"}
+              width={500}
+              height={500}
+              objectFit='cover'
+              alt={'nft-image'}
+            />
+          </div>
+
+          <TextArea
+            className={styles['input-description']}
+            name='NFTDescription'
+            placeholder='An Impressionist oil painting of sunflowers in a purple vase...'
+            value={NFTDescription || ''}
+            onChange={(e) => setNFTDescription(e.target.value)}
+            onSubmit={sendPrompt}
+            icon
+          />
+          <p className={styles['info-text']}>
+            Be as specific as you can and add plenty of clear details. Do not hold back on those details.
+          </p>
+
+          <div className={styles["example-chips-container"]}>
+            <Subtitle>
+              Examples
+            </Subtitle>
+            <div className={styles["example-chips-content"]}>
+              {mintPrompts.map((label, index) => (
+                <div key={index}>
+                  <Chip>{label}</Chip>
+                </div>
+              ))}
+            </div>
+          </div>
+        </SectionPanel>
+      </div>
+
+      {/* <InputComponent
           className={styles['input-nft-name']}
           name='NFTName'
           placeholder='Sunflowers painting'
           value={NFTName || ''}
           onChange={(e) => setNFTName(e.target.value)}
         /> */}
-
-        <div className={styles["image-content"]}>
-          <Image
-            src={"https://preview.redd.it/an-avocado-armchair-v0-uooc9auz6qs81.png?auto=webp&s=bf5227eca8895def1f055e6407a6f5da5dbee3a7"}
-            width={500}
-            height={500}
-            objectFit='cover'
-            alt={'nft-image'}
-          />
-        </div>
-
-        <TextArea
-          className={styles['input-description']}
-          name='NFTDescription'
-          placeholder='An Impressionist oil painting of sunflowers in a purple vase...'
-          value={NFTDescription || ''}
-          onChange={(e) => setNFTDescription(e.target.value)}
-          onSubmit={sendPrompt}
-          icon
-        />
-        <p className={styles['info-text']}>
-          Be as specific as you can and add plenty of clear details. Do not hold back on those details.
-        </p>
-      </div>
-
-      <div className={styles["example-chips-container"]}>
-        <Subtitle>
-          Examples
-        </Subtitle>
-        <div className={styles["example-chips-content"]}>
-          {mintPrompts.map((label, index) => (
-            <div key={index}>
-              <Chip>{label}</Chip>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>);
+    </div >);
 }
 
 export default MintUserNFT;
---
+++ src/placeholders/home-mint.placeholders.tsx
@@ -0,0 +1,5 @@
+export const generateLabels = {
+  title: 'AI-Powered NFT',
+  subtitle: 'Unleash your creativity and generate your own AI-powered NFT. Write anything you want and make it truly unique! ',
+  id: 'generative-art'
+};
