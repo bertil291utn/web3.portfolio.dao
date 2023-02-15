f6e7d4c9699c8426e58a23641f9283641686edcd
Bertil Tandayamo <bertilt4@gmail.com>
2023-02-15 11:20:01 -0500

    Display first and second panel back and forth

--- src/layouts/MintUserNFT.component.tsx
+++ src/layouts/MintUserNFT.component.tsx
@@ -9,13 +9,16 @@ import { mintPrompts } from '@placeholders/mint-prompts-examples.placeholders';
 import { countNumberLetters } from '@utils/common';
 import Image from 'next/image';
 import { useState } from 'react';
+import { AiOutlineArrowLeft } from 'react-icons/ai';
 import styles from './MintUserNFT.module.scss'
 
 const MintUserNFT = () => {
   const [NFTName, setNFTName] = useState<string>();
   const [NFTDescription, setNFTDescription] = useState<string>();
   const [currentActiveWindow, setCurrentActiveWindow] = useState<number>(1);
-  const [generatedImage, setgeneratedImage] = useState<string>("https://preview.redd.it/an-avocado-armchair-v0-uooc9auz6qs81.png?auto=webp&s=bf5227eca8895def1f055e6407a6f5da5dbee3a7");
+  const [generatedImage, setGeneratedImage] = useState<string>(
+    // "https://preview.redd.it/an-avocado-armchair-v0-uooc9auz6qs81.png?auto=webp&s=bf5227eca8895def1f055e6407a6f5da5dbee3a7"
+  );
 
   const sendPrompt = () => {
     if (countNumberLetters(NFTDescription || '') < 3) return;
@@ -31,23 +34,38 @@ const MintUserNFT = () => {
         <SectionPanel
           id={generateLabels.id}
           title={generateLabels.title}
-        // subtitle={generateLabels.subtitle}
+          subtitle={generateLabels.subtitle}
         >
 
+          {generatedImage && <div className={styles["image-content"]}>
+            <Image
+              src={generatedImage}
+              layout='fill'
+              objectFit='cover'
+              alt={'nft-image'}
+            />
+          </div>}
 
 
-          <TextArea
-            className={styles['input-description']}
-            name='NFTDescription'
-            placeholder='An Impressionist oil painting of sunflowers in a purple vase...'
-            value={NFTDescription || ''}
-            onChange={(e) => setNFTDescription(e.target.value)}
-            onSubmit={sendPrompt}
-            icon
-          />
-          {/* <p className={styles['info-text']}>
+          <div className={styles["text-area-container"]}>
+
+            <TextArea
+              className={styles['input-description']}
+              name='NFTDescription'
+              placeholder='An Impressionist oil painting of sunflowers in a purple vase...'
+              value={NFTDescription || ''}
+              onChange={(e) => setNFTDescription(e.target.value)}
+              onSubmit={sendPrompt}
+              icon
+            />
+            <p className={styles['info-text']}>
               Be as specific as you can and add plenty of clear details. Do not hold back on those details.
-            </p> */}
+            </p>
+          </div>
+
+          {generatedImage && <ButtonComponent onClick={() => setCurrentActiveWindow((prev) => ++prev)}>
+            go to mint
+          </ButtonComponent>}
 
           <div className={styles["example-chips-container"]}>
             <Subtitle>
@@ -63,41 +81,47 @@ const MintUserNFT = () => {
           </div>
         </SectionPanel>
 
-        <ButtonComponent onClick={() => setCurrentActiveWindow((prev) => ++prev)}>
-          next
-        </ButtonComponent>
+
 
       </div>}
 
-      {currentActiveWindow === 2 && <SectionPanel
-        id={mintLabels.id}
-        title={mintLabels.title}
-      // subtitle={mintLabels.subtitle}
-      >
-
-        {generatedImage &&
-          <div
-            className={styles['input-nft-name']}
+      {currentActiveWindow === 2 &&
+        <>
+          <AiOutlineArrowLeft className={styles['icon']} title='Go back' onClick={() => setCurrentActiveWindow((prev) => --prev)} />
+          <SectionPanel
+            id={mintLabels.id}
+            title={mintLabels.title}
+            subtitle={mintLabels.subtitle}
+            className={styles['section-panel']}
           >
 
-            <InputComponent
-              name='NFTName'
-              placeholder='Sunflowers painting'
-              value={NFTName || ''}
-              onChange={(e) => setNFTName(e.target.value)}
-            />
-          </div>
-        }
+            {generatedImage &&
+              <>
+                <div
+                  className={styles['input-nft-name']}
+                >
 
-        <div className={styles["image-content"]}>
-          <Image
-            src={generatedImage}
-            layout='fill'
-            objectFit='cover'
-            alt={'nft-image'}
-          />
-        </div>
-      </SectionPanel>}
+                  <InputComponent
+                    name='NFTName'
+                    placeholder='Sunflowers painting'
+                    value={NFTName || ''}
+                    onChange={(e) => setNFTName(e.target.value)}
+                  />
+                </div>
+
+                <div className={styles["image-content"]}>
+                  <Image
+                    src={generatedImage}
+                    layout='fill'
+                    objectFit='cover'
+                    alt={'nft-image'}
+                  />
+                </div>
+              </>
+            }
+          </SectionPanel>
+        </>
+      }
 
     </div >);
 }
